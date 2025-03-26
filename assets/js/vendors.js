c30fdb76bc5293b432129299769e31bbd00c8b83
krimatrivedi <krimatrivedi1@gmail.com>
2025-03-26 06:38:29 +0530

    changed URL of assets for vercel testing

--- assets/js/vendors.js
+++ assets/js/vendors.js
@@ -1,13 +1,13 @@
 const vendorScripts = [
-    "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
-    "assets/vendor/php-email-form/validate.js",
-    "assets/vendor/aos/aos.js",
-    "assets/vendor/waypoints/noframework.waypoints.js",
-    "assets/vendor/purecounter/purecounter_vanilla.js",
-    "assets/vendor/swiper/swiper-bundle.min.js",
-    "assets/vendor/glightbox/js/glightbox.min.js",
-    "assets/vendor/imagesloaded/imagesloaded.pkgd.min.js",
-    "assets/vendor/isotope-layout/isotope.pkgd.min.js"
+    "../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+    "../../assets/vendor/php-email-form/validate.js",
+    "../../assets/vendor/aos/aos.js",
+    "../../assets/vendor/waypoints/noframework.waypoints.js",
+    "../../assets/vendor/purecounter/purecounter_vanilla.js",
+    "../../assets/vendor/swiper/swiper-bundle.min.js",
+    "../../assets/vendor/glightbox/js/glightbox.min.js",
+    "../../assets/vendor/imagesloaded/imagesloaded.pkgd.min.js",
+    "../../assets/vendor/isotope-layout/isotope.pkgd.min.js"
   ];
   
   vendorScripts.forEach(src => {
